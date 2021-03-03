f69baacab0cbcec81b0ad4e978b2d7d00eeed770
Apcox89 <apcox89@gmail.com>
2021-03-03 15:30:42 -0600

    commit again

---
+++ src/app/recipe-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './recipe-page.component.html',
+    styleUrls: ['./recipe-page.component.css']
+})
+
+export class RecipePageComponent{ }
