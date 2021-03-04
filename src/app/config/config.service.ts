479ba6c71b451af690fe2b637fc7ebd878da50df
Apcox89 <apcox89@gmail.com>
2021-03-04 10:58:18 -0600

    complete commit

---
+++ src/app/config/config.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+
+export interface Config {
+    apiUrl: string;
+    date: any;
+  }
+
+@Injectable()
+export class ConfigService {
+
+    configUrl = 'assets/config.json';
+
+    constructor(private http: HttpClient) { }
+
+    getConfig() {
+        return this.http.get(this.configUrl);
+    }
+}  
