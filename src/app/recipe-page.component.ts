2fd7137c23da753c9eaf674b89f2d50b05f65ec3
Apcox89 <apcox89@gmail.com>
2021-03-04 11:56:14 -0600

    next style commit

--- src/app/recipe-page.component.ts
+++ src/app/recipe-page.component.ts
@@ -4,6 +4,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
     templateUrl: './recipe-page.component.html',
     styleUrls: ['./recipe-page.component.css']
+
+    //you may be able to configure your get req here to the db just like in the config type of setup
 })
 
 export class RecipePageComponent{ }
