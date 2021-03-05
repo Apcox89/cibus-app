0c8d106238ad8a90ddbd564176f1498ded6f6fa6
Apcox89 <apcox89@gmail.com>
2021-03-04 19:41:32 -0600

    market api initial config

---
+++ src/app/market.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+export interface Market {
+  name: string
+}
+
+@Injectable()
+export class MarketService {
+  constructor(private http: HttpClient) {}
+
+  getAllIngredients(): Observable<Market[]> {
+    return this.http.get<Market[]>('/api/ingredients/')
+  }
+
+  getIngredient(ingredientName: string): Observable<Market> {
+    return this.http.get<Market>('/api/ingredient/' + ingredientName)
+  }
+
+  postIngredient(ingredientName: Market): Observable<Market> {
+    return this.http.post<Market>('/api/ingredient/',ingredientName )
+  }
+
+  updateIngredient(ingredient: Market): Observable<void> {
+    return this.http.put<void>(
+      '/api/ingredient/' + ingredient.name,
+      ingredient
+    )
+  }
+
+  deleteIngredient(ingredientName: string) {
+    return this.http.delete('/api/ingredient/' + ingredientName)
+  }
+}
